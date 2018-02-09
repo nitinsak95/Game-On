da78921ce8347beb95454c8ad80fef579b7f178e
affixusimac1 <viveking@outlook.com>
2018-02-09 15:57:00 +0530

    About VC

--- Game On/HomeVC/AboutVC.swift
+++ Game On/HomeVC/AboutVC.swift
@@ -10,15 +10,25 @@ import UIKit
 
 class AboutVC: UIViewController {
 
+
+    @IBOutlet weak var arrowIcon: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.tintButtonImage(button: arrowIcon, image: (arrowIcon.imageView?.image)!, color: UIColor(hex: "ff5252"))
     }
 
     @IBAction func btCancel(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
     
+    func tintButtonImage(button:UIButton,image:UIImage,color:UIColor) {
+        let tintedImage = image.withRenderingMode(.alwaysTemplate)
+        button.setImage(tintedImage, for: .normal)
+        button.tintColor = color
+    }
 
 }
+
+
