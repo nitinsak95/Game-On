34c1369845d3e7ae41edc64b3f2336a9ce82993b
affixusimac1 <viveking@outlook.com>
2018-02-14 12:10:49 +0530

    MyMatchesChild VC

--- Game On/ChallengeVC/MyMatchesChildVC.swift
+++ Game On/ChallengeVC/MyMatchesChildVC.swift
@@ -9,21 +9,38 @@
 import UIKit
 import XLPagerTabStrip
 
-class MyMatchesChildVC: UIViewController, IndicatorInfoProvider {
+class MyMatchesChildVC: UIViewController, IndicatorInfoProvider, UITableViewDataSource, UITableViewDelegate {
     
+    let animals = ["", "", ""]
+    
+    @IBOutlet weak var tableView1: UITableView!
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
     
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo{
         return IndicatorInfo(title: "MY MATCHES")
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 110
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+    
+    
     
 }
